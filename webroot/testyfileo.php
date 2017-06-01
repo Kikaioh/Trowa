2e716774f9ff3e00513c980ee0407b69fc87b227
System Administrator <root@JOSEPHs-MacBook-Air.local>
2017-06-01 11:47:19 -0500

    Added cool stuff to the test file.

--- webroot/testyfileo.php
+++ webroot/testyfileo.php
@@ -1,7 +1,8 @@
 <?php
 	echo "Yeah, it's the testeriffic file, so what?<br>";
-	$wut = "I don't care.";
+	$wut = "I don't care what";
 	for($i=0; $i<10; $i++){
 		echo $wut."<br>";
+		echo "dat chicken butt<br>";
 	}
 ?>
