9d880af7c816636281744db5fbd7d5c33a1925e5
Joseph Garcia <kikaioh@gmail.com>
2017-06-02 10:06:24 -0500

    Let's cause some conflict.

--- webroot/testyfileo.php
+++ webroot/testyfileo.php
@@ -1,10 +1,5 @@
 <?php
 	echo "Yeah, it's the testeriffic file, so what?<br>";
-	$wut = "I don't care what";
-	for($i=0; $i<10; $i++){
-		echo $wut."<br>";
-		echo "dat chicken butt<br>";
-	}
 
-	echo "is it local, i dunnno, let's PARTAY";
+	echo "Let's cause some CONFLICT!!!11111!!!<br>";
 ?>
